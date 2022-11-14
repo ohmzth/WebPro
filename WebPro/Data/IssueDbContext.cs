bafb7c3fef101a8b54551525c8a2c71aff90b521
Mig113366 <neko113366@gmail.com>
2022-11-14 16:02:13 +0700

    update database เลย
    update database เลย

---
+++ WebPro/Data/IssueDbContext.cs
@@ -0,0 +1,14 @@
+﻿using WebPro.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebPro.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext>options) : base (options)
+        {
+            
+        }
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
---
+++ WebPro/Model/Issue.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebPro.Model
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public IssueType IssueType { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+
+    }
+    public enum Priority
+    {
+        Lo,Medium,High
+    }
+    public enum IssueType
+    {
+        Feature,Ducumentation,Bug
+    }
+}
