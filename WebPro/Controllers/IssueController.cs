3359bc12b0e1979cd3f91f332e1ca5d2afb764fa
Mig113366 <neko113366@gmail.com>
2022-11-15 13:41:31 +0700

    Create IssueController.cs

---
+++ WebPro/Controllers/IssueController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebPro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        //private readonly IssueDbContext _context;
+        //public IssueController(IssueDbContext context)
+        //{
+        //    _context = context;
+        //}
+    }
+}
